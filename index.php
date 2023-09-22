6294178828a132f3b36e632e09343322e9fda52e
Chalvinhuby55 <cjemihuby@gmail.com>
2023-09-22 16:15:30 +0900

    Upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+    <body>
+        <?php
+        // String
+        $name = 'Coding is fun';
+
+        // Integer
+        $name = 20;
+
+        // Float
+        $name = 20.5784;
+
+        // Boolean
+        
+
+        // Array
+        $names = array("Chalvin","Agus","Jekson");
+        echo $names[0];
+
+        
+        
+        ?>
+    </body>
+</html>
+
+
+
+        
+        
